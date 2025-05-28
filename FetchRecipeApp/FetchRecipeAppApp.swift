5638f19624b9fe4e89cd3c331a5e6282faf63052
Mohaimen Washik Rahman <mrahma62@asu.edu>
2025-05-27 17:46:22 -0700

    Initial Commit

---
+++ FetchRecipeApp/FetchRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeAppApp.swift
+//  FetchRecipeApp
+//
+//  Created by Mohaimen Washik on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
